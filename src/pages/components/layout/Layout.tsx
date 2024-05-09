5c0c207e221c02c59118ea4ad1bc21d1f829d74d
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-10 00:43:31 +0900

    feat: 메인 layout 컴포넌트 구현

---
+++ src/pages/components/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+import Gnb from '../gnb/Gnb';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Gnb />
+      <main className='p-[0_2rem_0_2rem] lg:max-w-[78%] lg:m-[0_2rem_0_auto]'>{children}</main>
+    </>
+  );
+}
