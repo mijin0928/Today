c953b377ef96486490a1b4c62410f6a5ef24710a
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-10 04:12:11 +0900

    feat: useSetInterval 훅 적용 (#25)

--- src/pages/hooks/useSetInterval.ts
+++ src/pages/hooks/useSetInterval.ts
@@ -1,11 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function useSetInterval(callback: () => void, seconds: number) {
+  const timerRef = useRef<string | number | NodeJS.Timeout | undefined>();
+
   useEffect(() => {
-    const timer = setInterval(callback, seconds);
+    timerRef.current = setInterval(callback, seconds);
 
     return () => {
-      clearInterval(timer);
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
     };
   }, []);
+
+  return timerRef;
 }
