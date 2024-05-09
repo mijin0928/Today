00b17347890eb25930932201ec690105eff65be3
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-09 23:45:54 +0900

    fix: title 글자가 나타난 뒤 gnb 컴포넌트 나오도록 수정 (#10)
    * chore: Title 컴포넌트 폴더 생성 후 컴포넌트 파일 이동
    
    * fix: title 글자가 나타난 뒤 gnb 컴포넌트 나오도록 수정

--- src/pages/components/title/Title.tsx
+++ src/pages/components/title/Title.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+import Gnb from '../gnb/Gnb';
 
 export default function Title() {
   const [title, setTitle] = useState('');
+  const [end, setEnd] = useState(false);
   const TITLE: string = 'TODAY IS ...';
   let count = 0;
   let total = '';
@@ -11,17 +13,23 @@ export default function Title() {
       if (count < TITLE.length) {
         total += TITLE[count++];
         setTitle(total);
+      } else {
+        clearTimeout(timer);
+        setEnd(true);
       }
     }, 300);
-   
+
     return () => {
       clearInterval(timer);
     };
   }, []);
 
   return (
-    <h1 className='flex justify-center items-center h-screen font-fredoka text-5xl md:text-8xl lg:text-9xl text-primary'>
-      {title}
-    </h1>
+    <>
+      <h1 className='flex justify-center items-center h-screen font-fredoka text-5xl md:text-8xl lg:text-9xl text-primary'>
+        {title}
+      </h1>
+      {end && <Gnb />}
+    </>
   );
 }
