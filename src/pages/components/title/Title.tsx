97010d36d84e188d7c2e1bf94a8ef4a7de5bc55c
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-09 23:59:08 +0900

    fix: Circle 컴포넌트에 있는 스타일을 title에 적용 (#15)
    * chore: Title 컴포넌트 폴더 생성 후 컴포넌트 파일 이동
    
    * fix: title 글자가 나타난 뒤 gnb 컴포넌트 나오도록 수정
    
    * fix: 태블릿, 모바일에서 햄버거메뉴 안나오는 오류 수정
    
    * fix: Circle 컴포넌트에 있는 스타일을 title에 적용

--- src/pages/components/title/Title.tsx
+++ src/pages/components/title/Title.tsx
@@ -27,7 +27,7 @@ export default function Title() {
   return (
     <>
       {end && <Gnb />}
-      <h1 className='flex justify-center items-center h-screen font-fredoka text-5xl md:text-8xl lg:text-9xl text-primary'>
+      <h1 className='flex justify-center items-center h-screen font-fredoka text-5xl md:text-8xl lg:text-9xl text-primary before:absolute before:z-[-1] before:size-[10rem] before:md:size-[15rem] before:lg:size-[20rem] before:left-[calc(50%_-_5rem)] before:md:left-[calc(50%_-_7.5rem)] before:lg:left-[calc(50%_-_10rem)] before:top-[50%] before:rounded-full before:animate-rotate before:bg-white before:origin-top'>
         {title}
       </h1>
     </>
