e0be349643f7e0259748889be812ebbd90f1bf0f
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-21 01:48:41 +0900

    feat: useWeather 훅 추가

--- src/pages/components/weatherInfo/WeatherInfo.tsx
+++ src/pages/components/weatherInfo/WeatherInfo.tsx
@@ -1,34 +1,12 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { BASE_URL, KEY, WEATHER } from '@/constant/constant';
-import { useQuery } from '@tanstack/react-query';
-import { getWeather } from '../../api/api';
-
-interface PositionError {
-  readonly code: number;
-  readonly message: string;
-}
+import { WEATHER } from '@/constant/constant';
+import { useWeather } from '../../hooks/useWeather';
 
 export default function WeatherInfo() {
   const [description, setDescription] = useState('');
   const [id, setId] = useState(0);
-  const [weather, setWeather] = useState('');
-
-  const success = (pos: GeolocationPosition) => {
-    const latitude = pos.coords.latitude;
-    const longitude = pos.coords.longitude;
-
-    setWeather(`${BASE_URL}/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}&units=metric`);
-  };
-
-  const error = (err: PositionError) => {
-    console.error(`${err.code}:${err.message}`);
-  };
-
-  const { data: weatherData, isLoading: weatherDataLoading } = useQuery({
-    queryKey: [weather],
-    queryFn: () => getWeather(weather),
-  });
+  const { success, error, weatherData, weatherDataLoading } = useWeather();
 
   useEffect(() => {
     if (weatherData) {
@@ -46,33 +24,33 @@ export default function WeatherInfo() {
   if (weatherDataLoading) return <div>로딩중</div>;
 
   return (
-    <div className='relative md:flex md:items-center md:gap-20'>
-      <Image
-        className='w-[14rem] h-[14rem] md:w-[20rem] md:h-[20rem] m-auto md:m-0 rounded-full'
-        src={`/icons/${weatherData.weather[0].icon}.gif`}
-        width={0}
-        height={0}
-        alt='오늘의 날씨'
-      />
-      <div className='text-primary text-center md:text-left'>
-        <div className='relative'>
+    <>
+      <div className='relative md:flex md:items-center md:gap-20'>
+        <Image
+          className='w-[14rem] h-[14rem] md:w-[20rem] md:h-[20rem] m-auto md:m-0 rounded-full'
+          src={`/icons/${weatherData.weather[0].icon}.gif`}
+          width={0}
+          height={0}
+          alt='오늘의 날씨'
+        />
+        <div className='text-primary text-center md:text-left'>
+          <div className='relative'>
             <p className='absolute left-[calc(50%_-_5.1rem)] md:left-0 min-[1200px]:inline-block text-[3.5rem] md:text-[4.3rem] text-white [clip-path:polygon(0_70%,_100%_50%,_100%_100%,_0%_100%)] animate-skew'>
-            {weatherData.name.split('-')[0]}
-          </p>
-          <p className='min-[1200px]:inline-block text-[3.5rem] md:text-[4.3rem]'>
-            {weatherData.name.split('-')[0]}
-          </p>
-           <p className='absolute overflow-hidden min-[1200px]:inline-block left-[calc(50%_-_6.3rem)] md:left-[auto] h-[3rem] md:text-[4.5rem] min-[1200px]:ml-4 text-[3.5rem] text-white animate-skew'>
-            {weatherData.main.temp}
-            <span className='text-[4rem] md:text-[4.3rem]'>&#8451;</span>
-          </p>
-          <p className='min-[1200px]:inline-block md:text-[4.5rem] min-[1200px]:ml-4 text-[3.5rem]'>
-            {weatherData.main.temp}
-            <span className='text-[4rem] md:text-[4.3rem]'>&#8451;</span>
-          </p>
+              {weatherData.name.split('-')[0]}
+            </p>
+            <p className='min-[1200px]:inline-block text-[3.5rem] md:text-[4.3rem]'>{weatherData.name.split('-')[0]}</p>
+            <p className='absolute overflow-hidden min-[1200px]:inline-block left-[calc(50%_-_6.3rem)] md:left-[auto] h-[3rem] md:text-[4.5rem] min-[1200px]:ml-4 text-[3.5rem] text-white animate-skew'>
+              {weatherData.main.temp}
+              <span className='text-[4rem] md:text-[4.3rem]'>&#8451;</span>
+            </p>
+            <p className='min-[1200px]:inline-block md:text-[4.5rem] min-[1200px]:ml-4 text-[3.5rem]'>
+              {weatherData.main.temp}
+              <span className='text-[4rem] md:text-[4.3rem]'>&#8451;</span>
+            </p>
+          </div>
+          <p className='mt-4 text-[2.5rem] md:text-[2.8rem] break-keep'>{description}</p>
         </div>
-        <p className='mt-4 text-[2.5rem] md:text-[2.8rem] break-keep'>{description}</p>
       </div>
-    </div>
+    </>
   );
 }
