0f3b7b789034469e8633e0ce1a99a19f27f00afd
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-21 01:04:51 +0900

    weatherInfo 컴포넌트 구현 (#31)
    * feat: 현재 위치를 가져와 날씨 api를 연동
    
    * design: ui 구현
    
    Please enter the commit message for your changes. Lines starting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css';
 import { nanum_Gothic, fredoka, poor_Story, cls } from '@/styles/font';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+  
   return (
     <div className={cls(nanum_Gothic.className, fredoka.variable, poor_Story.variable)}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </div>
   );
 }
