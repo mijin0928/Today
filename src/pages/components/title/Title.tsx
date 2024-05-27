ef95fcad4cbcb8f5e395243fcdbbdaba62368758
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-28 01:17:01 +0900

    fix: 충돌 해결

--- src/pages/components/title/Title.tsx
+++ src/pages/components/title/Title.tsx
@@ -1,12 +1,6 @@
-<<<<<<< title
 import { useState, useRef } from 'react';
 import Gnb from '../gnb/Gnb';
 import { useSetInterval } from '@/pages/hooks/useSetInterval';
-=======
-import { useState } from 'react';
-import Gnb from '../gnb/Gnb';
-import useSetInterval from '@/pages/hooks/useSetInterval';
->>>>>>> main
 
 export default function Title() {
   const [title, setTitle] = useState('');
@@ -15,18 +9,8 @@ export default function Title() {
   const countRef = useRef(0);
   const totalRef = useRef('');
 
-<<<<<<< title
   useSetInterval(() => {
     countRef.current < TITLE.length ? setTitle((totalRef.current += TITLE[countRef.current++])) : setEnd(true);
-=======
-  const timerRef = useSetInterval(() => {
-    setTitle((total += TITLE[count++]));
-
-    if (count >= TITLE.length) {
-      clearInterval(timerRef.current);
-      setEnd(true);
-    }
->>>>>>> main
   }, 300);
 
   return (
--- src/pages/hooks/useSetInterval.ts
+++ src/pages/hooks/useSetInterval.ts
@@ -1,26 +1,13 @@
 import { useEffect, useRef } from 'react';
-<<<<<<< title
 
 export function useSetInterval(callback: () => void, seconds: number) {
   const timerRef = useRef<string | number | NodeJS.Timeout | undefined>();
 
-=======
-
-export default function useSetInterval(callback: () => void, seconds: number) {
-  const timerRef = useRef<string | number | NodeJS.Timeout | undefined>();
-
->>>>>>> main
   useEffect(() => {
     timerRef.current = setInterval(callback, seconds);
 
     return () => {
-<<<<<<< title
       clearInterval(timerRef.current);
-=======
-      if (timerRef.current) {
-        clearInterval(timerRef.current);
-      }
->>>>>>> main
     };
   }, []);
 
