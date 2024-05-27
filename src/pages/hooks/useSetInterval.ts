2e4694c3c9d2853462352a43303bbcf6cae0ae2c
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-28 01:32:02 +0900

    refactor: useSetInterval 훅 리팩토링 (#44)
    * feat: useSetInterval 훅 적용
    
    * fix: let 변수를 useRef로 변경
    
    * refactor: useSetInterval 훅 리팩토링

--- src/pages/hooks/useSetInterval.ts
+++ src/pages/hooks/useSetInterval.ts
@@ -2,7 +2,9 @@ import { useEffect, useRef } from 'react';
 
 export function useSetInterval(callback: () => void, seconds: number) {
   const timerRef = useRef<string | number | NodeJS.Timeout | undefined>();
-
+  const countRef = useRef(0);
+  const totalRef = useRef('');
+  
   useEffect(() => {
     timerRef.current = setInterval(callback, seconds);
 
@@ -11,5 +13,5 @@ export function useSetInterval(callback: () => void, seconds: number) {
     };
   }, []);
 
-  return timerRef;
+  return { countRef, totalRef };
 }
