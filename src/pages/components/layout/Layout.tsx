a5e14eb1affaa79a635a08f731eff3801e95d0e2
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-05-29 00:03:23 +0900

    design: pc사이즈일 때 padding 값 0으로 수정

--- src/pages/components/layout/Layout.tsx
+++ src/pages/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Gnb />
-      <main className='p-[0_2rem_0_2rem] lg:max-w-[78%] lg:m-[0_2rem_0_auto]'>{children}</main>
+      <main className='p-[0_2rem_0_2rem] lg:p-0 lg:max-w-[78%] lg:m-[0_2rem_0_auto]'>{children}</main>
     </>
   );
 }
