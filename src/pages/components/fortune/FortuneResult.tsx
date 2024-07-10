3cfd8bd2dfc4a6747a373ecdf4987a301053cd0c
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-07-11 00:15:53 +0900

    feat: 처음으로 버튼 구현

--- src/pages/components/fortune/FortuneResult.tsx
+++ src/pages/components/fortune/FortuneResult.tsx
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 import { useEffect, useState } from 'react';
-
+import Image from 'next/image';
+import Fortune from './Fortune';
 interface Fortune {
   id: string;
   result: string;
@@ -25,7 +26,12 @@ export async function getStaticProps() {
 
 export default function FortuneResult({ fortune }: { fortune: Fortune[] }) {
   const [result, setResult] = useState(0);
+  const [reset, setReset] = useState(false);
   const random = Math.floor(Math.random() * fortune.length);
+
+  const handleResetClick = () => {
+    setReset(true);
+  };
 
   useEffect(() => {
     setResult(random);
@@ -33,13 +39,34 @@ export default function FortuneResult({ fortune }: { fortune: Fortune[] }) {
 
   return (
     <>
-      <div className='relative flex justify-center items-center h-screen before:absolute before:top-2/4 before:left-2/4 before:translate-x-[-50%] before:translate-y-[-50%] before:w-screen before:h-[90vh] before:bg-paper before:bg-contain before:bg-no-repeat before:bg-center before:z-[-1]'>
-        {fortune.map((item, idx) => {
-          if (idx === result) {
-            return <p key={item.id} className='p-4 text-primary text-justify text-[1.25rem] bg-white max-sm:text-[1rem] max-md:max-h-[200px] max-sm:max-h-[80px] max-md:overflow-y-auto scrollbar-thumb-primary scrollbar-track-transparent scrollbar-thin max-w-[45%]'>{item.result}</p>;
-          }
-        })}
-      </div>
+      {reset ? (
+        <Fortune />
+      ) : (
+        <>
+          <div className='relative flex justify-center items-center h-screen before:absolute before:top-2/4 before:left-2/4 before:translate-x-[-50%] before:translate-y-[-50%] before:w-screen before:h-[90vh] before:bg-paper before:bg-contain before:bg-no-repeat before:bg-center before:z-[-1]'>
+            {fortune.map((item, idx) => {
+              if (idx === result) {
+                return (
+                  <p
+                    key={item.id}
+                    className='p-4 text-primary text-[1.25rem] bg-white max-sm:text-[1rem] max-md:max-h-[200px] max-sm:max-h-[80px] max-md:overflow-y-auto scrollbar-thumb-primary scrollbar-track-transparent scrollbar-thin max-w-[45%]'
+                  >
+                    {item.result}
+                  </p>
+                );
+              }
+            })}
+          </div>
+          <Image
+            className='absolute bottom-[2rem] right-[3rem] cursor-pointer max-sm:w-[60px]'
+            width={100}
+            height={100}
+            src='/reset.png'
+            alt='처음으로'
+            onClick={handleResetClick}
+          />
+        </>
+      )}
     </>
   );
 }
