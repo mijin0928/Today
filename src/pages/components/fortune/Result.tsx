eb1445099114c28cc0fef4cce87ea2694e286002
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-07-16 21:19:31 +0900

    button태그 수정 (#57)
    * design: ui 구현
    
    * feat: mouse이벤트 구현
    
    * feat: mongodb 연결 후 데이터 가져오기
    
    * feat: 운세 결과 출력
    
    * feat: 처음으로 버튼 구현
    
    * feat: 타이머 함수로 일정 시간이 지나면 운세 결과 보여주기
    
    * fix: Image를 button태그로 수정
    
    * feat: fortune 페이지 구현
    
    * feat: 동적 라우팅 처리
    
    * feat: mongodb 연동해서 운세 결과 보여주기
    
    * feat: 로딩 구현
    
    * fix: button 태그 수정

--- src/pages/components/fortune/Result.tsx
+++ src/pages/components/fortune/Result.tsx
@@ -47,10 +47,12 @@ export default function Result() {
         })}
       </div>
       <button
-        className='w-[6.25rem] h-[6.25rem] bg-reset bg-no-repeat bg-center bg-cover absolute bottom-[2rem] right-[3rem] max-sm:w-[3.75rem] max-sm:h-[3.75rem]'
+        className='w-[6.25rem] h-[6.25rem] bg-reset bg-no-repeat bg-center bg-cover absolute bottom-[2rem] right-[3rem] max-sm:w-[3.75rem] max-sm:h-[3.75rem] indent-[100%] overflow-hidden whitespace-nowrap'
         type='button'
         onClick={handleResetClick}
-      />
+      >
+        처음으로
+      </button>
     </>
   );
 }
