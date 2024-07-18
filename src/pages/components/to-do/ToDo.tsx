7e63b6e31f7c8203da0b213c5be6ce12c1da114b
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-07-19 00:19:02 +0900

    fix: checkbox 오류 수정

--- src/pages/components/to-do/ToDo.tsx
+++ src/pages/components/to-do/ToDo.tsx
@@ -5,7 +5,7 @@ import { Todo } from '@/type/type';
 export default function ToDo() {
   const [value, setValue] = useState('');
   const [todo, setTodo] = useState<Todo[]>([]);
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   const [hasValue, setHasValue] = useState(false);
   const [a, seta] = useState(false);
   const isRemove = a ? 'animate-up' : 'animate-down';
@@ -45,22 +45,22 @@ export default function ToDo() {
       {hasValue && (
         <div>
           <ul className='mt-12 m-[0_auto] w-[50rem]'>
-            {todo.map((todo: Todo) => (
+            {todo.map((todo: Todo, idx) => (
               <li
                 className={`relative mt-4 first:mt-0 text-[1.25rem] text-primary bg-white px-5 py-4 rounded-3xl ${isRemove}`}
                 key={todo.id}
               >
                 <input
-                  className='absolute left-8 top-[50%] translate-y-[-50%] appearance-none w-11 h-11 bg-checkbox bg-contain bg-no-repeat cursor-pointer checked:bg-checkboxOn'
-                  id='todoCheck'
-                  name='todoCheck'
+                  className='absolute left-8 top-[50%] translate-y-[-50%] appearance-none w-10 h-10 bg-checkbox bg-contain bg-no-repeat cursor-pointer checked:bg-checkboxOn'
+                  id={`check${idx + 1}`}
+                  name={`check${idx + 1}`}
                   type='checkbox'
                 />
-                <label className='ml-20 cursor-pointer' htmlFor='todoCheck'>
+                <label className='ml-20 cursor-pointer' htmlFor={`check${idx + 1}`}>
                   {todo.text}
                 </label>
                 <button
-                  className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-[50px] h-[50px] indent-[100%] whitespace-nowrap bg-remove bg-no-repeat bg-contain hover:bg-removeOn'
+                  className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-11 h-11 indent-[100%] whitespace-nowrap bg-remove bg-no-repeat bg-contain hover:bg-removeOn'
                   type='button'
                   onClick={() => handleRemoveClick(todo.id)}
                 >
