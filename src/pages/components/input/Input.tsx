9dc042bc515c686c40a29f96bbae6a0f44b3e7c0
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-07-30 18:18:31 +0900

    design: 사이즈에 따른 input 위치 수정

--- src/pages/components/input/Input.tsx
+++ src/pages/components/input/Input.tsx
@@ -4,7 +4,7 @@ import Button from './Button';
 
 export default function Input({ type, value, onClick, onChange, onKeyDown }: InputProps) {
   return (
-    <div className='flex justify-center items-center 2xl:justify-start]'>
+    <div className='flex justify-center items-center 2xl:justify-start'>
       <input
         className='w-[37.5rem] h-[3.55rem] pl-6 mr-6 outline-none text-[1.18rem] shadow-[2px_2px_2px_#e9d6d64b] text-primary placeholder:text-[1.18rem] max-sm:text-base'
         onChange={onChange}
@@ -12,11 +12,7 @@ export default function Input({ type, value, onClick, onChange, onKeyDown }: Inp
         value={value}
         type='text'
       />
-      {type === 'button' ? (
-        <Button value={value} onClick={onClick} />
-      ) : (
-        <Dropdown value={value} onClick={onClick} />
-      )}
+      {type === 'button' ? <Button value={value} onClick={onClick} /> : <Dropdown value={value} onClick={onClick} />}
     </div>
   );
 }
