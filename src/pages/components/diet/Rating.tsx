8d56bd1c5fbe252016e82d636bd883c969e225fa
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-08-04 05:17:20 +0900

    feat: 별점 클릭시 이벤트 구현

--- src/pages/components/diet/Rating.tsx
+++ src/pages/components/diet/Rating.tsx
@@ -2,35 +2,42 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 export default function Rating() {
-  const [rating, setRating] = useState(0);
-  const [review, setReview] = useState('');
+  const [hoverRating, setHoverRating] = useState(0);
+  const [hoverReview, setHoverReview] = useState('');
+  const [selectedRating, setSelectedRating] = useState(0);
+  const [selectedReview, setSelectedReview] = useState('');
+  const isSelectedReview = hoverRating < selectedRating ? selectedReview : hoverReview;
+
+  const handleRatingLeave = () => {
+    setHoverRating(0);
+    setHoverReview('');
+  };
 
   const handleRatingEnter = (idx: number) => {
-    setRating(idx + 1);
+    setHoverRating(idx + 1);
 
     switch (idx + 1) {
       case 1:
-        setReview('맛없어요');
+        setHoverReview('맛없어요');
         break;
       case 2:
-        '별로예요';
-        setReview('별로예요');
+        setHoverReview('별로예요');
         break;
       case 3:
-        setReview('무난해요');
+        setHoverReview('무난해요');
         break;
       case 4:
-        setReview('맛있어요');
+        setHoverReview('맛있어요');
         break;
       case 5:
-        setReview('정말 맛있어요');
+        setHoverReview('정말 맛있어요');
         break;
     }
   };
 
-  const handleRatingLeave = () => {
-    setRating(0);
-    setReview('');
+  const handleRatingClick = (idx: number) => {
+    setSelectedRating(idx + 1);
+    setSelectedReview(hoverReview);
   };
 
   return (
@@ -41,14 +48,15 @@ export default function Rating() {
           <Image
             className='cursor-pointer'
             key={idx}
-            src={idx < rating ? '/star-on.png' : '/star.png'}
+            src={idx < hoverRating || idx < selectedRating ? '/star-on.png' : '/star.png'}
             width={35}
             height={35}
             alt='별점'
             onMouseEnter={() => handleRatingEnter(idx)}
+            onClick={() => handleRatingClick(idx)}
           />
         ))}
-      <p className='ml-1'>{review}</p>
+      <p className='ml-1'>{isSelectedReview}</p>
     </div>
   );
 }
