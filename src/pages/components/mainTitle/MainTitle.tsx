ef4feb1acf4c9535965e2bfdcbfd589d4d2d013c
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-09-07 20:55:31 +0900

    fix: 원 위치 틀어짐 수정 (#72)

--- src/pages/components/mainTitle/MainTitle.tsx
+++ src/pages/components/mainTitle/MainTitle.tsx
@@ -15,8 +15,8 @@ export default function MainTitle({ title }: { title: string }) {
   }, 400);
 
   return (
-    <h2 className='pb-10 text-[3.5rem] md:text-[5rem] font-poor_Story text-primary before:absolute before:left-[11.8rem] before:md:left-[16.3rem] before:top-[0.5rem] before:md:top-[0.7rem] before:z-[-1] before:w-[5rem] before:md:w-[6.5rem] before:h-[5rem] before:md:h-[6.5rem] before:rounded-[2.8rem] before:bg-white before:animate-rotate'>
-      오늘의 {title} <span className='font-fredoka'>{dot}</span>
+    <h2 className='relative inline-block pb-10 text-[3rem] sm:text-[5rem] font-poor_Story text-primary before:absolute before:right-0 before:sm:right-[4.9rem] before:top-0 before:sm:top-[0.5rem] before:z-[-1] before:w-[5rem] before:sm:w-[6.5rem] before:h-[5rem] before:sm:h-[6.5rem] before:rounded-[2.8rem] before:bg-white before:animate-rotate'>
+      오늘의 {title} <span className='hidden sm:inline-block w-[3.25rem] leading-4 font-fredoka'>{dot}</span>
     </h2>
   );
 }
