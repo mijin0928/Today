cb6eedc0df29bdb5466e79379abfa689cfb2d6cc
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-09-07 21:34:53 +0900

    fix: 레이아웃 틀어짐 수정

--- src/pages/components/weatherDetail/WeatherDetail.tsx
+++ src/pages/components/weatherDetail/WeatherDetail.tsx
@@ -44,16 +44,16 @@ export default function WeatherDetail() {
   ];
 
   return (
-    <ul className='flex flex-wrap mt-20 gap-5'>
+    <ul className='flex flex-wrap mt-16 gap-5'>
       {WEATHER.map((weather, i) => (
         <li
-          className=' px-5 py-5 rounded-3xl w-[calc(50%_-_1.25rem)] md:w-[calc(33.3%_-_1.25rem)] xl:w-[calc(20%_-_1.25rem)] h-[auto] shadow-[3px_3px_3px_rgba(0,0,0,0.163)] bg-white'
+          className='px-5 py-5 rounded-3xl h-auto w-[calc(50%_-_0.62rem)] md:w-[calc(33.3%_-_0.82rem)] 2xl:w-[calc(25%_-_5rem)] shadow-[3px_3px_3px_rgba(0,0,0,0.163)] bg-white'
           key={i}
         >
           <Image className='m-[auto]' src={weather.src} width={100} height={100} alt={weather.alt} />
           <p className='mt-5 text-[2rem] md:text-[3rem] text-center text-primary leading-none'>
             {weather.data}
-            <span className='block md:inline-block text-[1.5rem] md:text-[2rem]'>{weather.text}</span>
+            <span className='inline-block text-[1.5rem] md:text-[2rem] break-words'>{weather.text}</span>
           </p>
         </li>
       ))}
