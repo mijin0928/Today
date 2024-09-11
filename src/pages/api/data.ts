e717d8f886d5bdd80cdc5b99159e8ccfa19b533a
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-09-11 16:18:59 +0900

    Fortune 컴포넌트 수정 (#82)
    * design: ui 구현
    
    * feat: mouse이벤트 구현
    
    * feat: mongodb 연결 후 데이터 가져오기
    
    * feat: 운세 결과 출력
    
    * feat: 처음으로 버튼 구현
    
    * feat: 타이머 함수로 일정 시간이 지나면 운세 결과 보여주기
    
    * fix: Image를 button태그로 수정
    
    * feat: fortune 페이지 구현
    
    * feat: 동적 라우팅 처리
    
    * feat: mongodb 연동해서 운세 결과 보여주기
    
    * refactor: 컴포넌트 분리
    
    * fix: 커서에  손이미지가 따라다니지 않는 오류 수정
    
    * feat: 환경변수 생성

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -2,9 +2,11 @@ import { MongoClient } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function data(req: NextApiRequest, res: NextApiResponse) {
-  const client = await MongoClient.connect('mongodb+srv://mijin:qlalf0928@cluster0.tdkhkxz.mongodb.net/');
+  const client = await MongoClient.connect(
+    `mongodb+srv://mijin:${process.env.REACT_APP_MONGODB_PW}@cluster0.tdkhkxz.mongodb.net/`
+  );
   const db = client.db('today');
-  
+
   if (req.method === 'GET') {
     const result = await db
       .collection('fortune')
--- src/pages/fortune/index.tsx
+++ src/pages/fortune/index.tsx
@@ -1,12 +1,12 @@
 import Layout from '../components/layout/Layout';
 import MainTitle from '../components/mainTitle/MainTitle';
-import Intro from '../components/fortune/Intro';
+import Hand from '../components/fortune/Hand';
 
 export default function Fortune() {
   return (
     <Layout>
       <MainTitle title='운세' />
-      <Intro />
+      <Hand />
     </Layout>
   );
 }
