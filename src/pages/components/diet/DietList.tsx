189d597ff3d568f035f126fc23472e3d356731bb
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-09-23 01:22:27 +0900

    design: ui 틀어지는 부분 수정

--- src/pages/components/diet/DietList.tsx
+++ src/pages/components/diet/DietList.tsx
@@ -77,7 +77,6 @@ export default function DietList() {
               updateItem={updateItem}
             />
           </div>
-          <Count filterItem={filterItem} />
         </div>
       )}
     </>
