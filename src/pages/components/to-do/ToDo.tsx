cf42faf4e9623925d0974c468ba13249d1358d09
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-16 15:24:27 +0900

    feat: 삭제 기능 구현

--- src/pages/components/to-do/ToDo.tsx
+++ src/pages/components/to-do/ToDo.tsx
@@ -6,6 +6,7 @@ export default function ToDo() {
   const [value, setValue] = useState('');
   const [todo, setTodo] = useState<Todo[]>([]);
   const [count, setCount] = useState(1);
+  const [hasValue, setHasValue] = useState(false);
 
   const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -15,12 +16,17 @@ export default function ToDo() {
     setCount(count + 1);
     setTodo([...todo, { id: count, text: value }]);
     setValue('');
+    setHasValue(true);
   };
 
   const handleEnterKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter') {
       handleAddClick();
     }
+  };
+
+  const handleRemoveClick = () => {
+    setTodo([]);
   };
 
   return (
@@ -32,11 +38,24 @@ export default function ToDo() {
         onChange={handleValueChange}
         onKeyDown={handleEnterKeyDown}
       />
-      <ul>
-        {todo.map((todo: Todo) => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
+      {hasValue && (
+        <div>
+          <ul className='mt-12 m-[0_auto] w-[50rem]'>
+            {todo.map((todo: Todo) => (
+              <li className='relative mt-4 text-[1.25rem] text-primary bg-white px-5 py-4 rounded-3xl' key={todo.id}>
+                {todo.text}
+                <button
+                  className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-[50px] h-[50px] indent-[100%] whitespace-nowrap bg-remove bg-no-repeat bg-contain'
+                  type='button'
+                  onClick={handleRemoveClick}
+                >
+                  삭제하기
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
 }
