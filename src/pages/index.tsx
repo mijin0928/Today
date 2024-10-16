61e1d878acd20fe8e3dcfb1944de388c90ddbadd
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-16 15:23:46 +0900

    feat: 할일 목록 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Title from './components/title/Title';
-
+import ToDo from './components/to-do/ToDo';
 export default function Home() {
   return (
     <>
@@ -10,7 +10,8 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Title />
+      {/* <Title /> */}
+      <ToDo />
     </>
   );
 }
