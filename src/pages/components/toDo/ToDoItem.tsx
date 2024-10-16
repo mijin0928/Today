20e42692a108798706f0a20873c85d3d67685581
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-17 01:06:06 +0900

    fix: 타입 오류 수정

--- src/pages/components/toDo/ToDoItem.tsx
+++ src/pages/components/toDo/ToDoItem.tsx
@@ -10,7 +10,7 @@ export default function ToDoItem({ todo, handleDeleteClick, handleCheckChange, h
     <div className={`flex-col items-center gap-10 2xl:flex-row-reverse 2xl:justify-end 2xl:gap-20 flex ${isVisible}`}>
       <Count todo={todo} />
       <ul className='xl:shrink-0 overflow-y-auto w-full md:w-[43.75rem] h-[21.8rem] scrollbar-thumb-primary scrollbar-track-transparent scrollbar-thin'>
-        {todo.map((todo, idx) => (
+        {todo && todo.map((todo, idx) => (
           <li className={`relative mt-4 first:mt-0 bg-white px-5 py-4 rounded-3xl`} key={todo.id}>
             <Check todo={todo} idx={idx} handleCheckChange={handleCheckChange} />
             <Delete todo={todo} handleDeleteClick={handleDeleteClick} />
