3a47e931747c79238fe58ff1133ad02b2b588a20
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-16 15:25:42 +0900

    feat: 삭제 버튼 hover 시 효과

--- src/pages/components/to-do/ToDo.tsx
+++ src/pages/components/to-do/ToDo.tsx
@@ -7,6 +7,8 @@ export default function ToDo() {
   const [todo, setTodo] = useState<Todo[]>([]);
   const [count, setCount] = useState(1);
   const [hasValue, setHasValue] = useState(false);
+  const [a, seta] = useState(false);
+  const isRemove = a ? 'animate-up' : 'animate-down';
 
   const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -17,6 +19,7 @@ export default function ToDo() {
     setTodo([...todo, { id: count, text: value }]);
     setValue('');
     setHasValue(true);
+    seta(true);
   };
 
   const handleEnterKeyDown = (e: React.KeyboardEvent) => {
@@ -25,8 +28,9 @@ export default function ToDo() {
     }
   };
 
-  const handleRemoveClick = () => {
-    setTodo([]);
+  const handleRemoveClick = (id: number) => {
+    const updateTodo = todo.filter((item) => item.id !== id);
+    setTodo(updateTodo);
   };
 
   return (
@@ -43,14 +47,14 @@ export default function ToDo() {
           <ul className='mt-12 m-[0_auto] w-[50rem]'>
             {todo.map((todo: Todo) => (
               <li
-                className='relative mt-4 first:mt-0 text-[1.25rem] text-primary bg-white px-5 py-4 rounded-3xl animate-up'
+                className={`relative mt-4 first:mt-0 text-[1.25rem] text-primary bg-white px-5 py-4 rounded-3xl ${isRemove}`}
                 key={todo.id}
               >
                 {todo.text}
                 <button
-                  className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-[50px] h-[50px] indent-[100%] whitespace-nowrap bg-remove bg-no-repeat bg-contain'
+                  className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-[50px] h-[50px] indent-[100%] whitespace-nowrap bg-remove bg-no-repeat bg-contain hover:bg-removeOn'
                   type='button'
-                  onClick={handleRemoveClick}
+                  onClick={() => handleRemoveClick(todo.id)}
                 >
                   삭제하기
                 </button>
