bb328f674f993f8179a06ecde27eb30b766442a9
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-16 15:26:37 +0900

    feat: 남은 할일 개수 보여주기

--- src/pages/components/to-do/ToDo.tsx
+++ src/pages/components/to-do/ToDo.tsx
@@ -1,25 +1,26 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState, useRef } from 'react';
 import Input from '../input/Input';
 import { Todo } from '@/type/type';
 
 export default function ToDo() {
   const [value, setValue] = useState('');
   const [todo, setTodo] = useState<Todo[]>([]);
-  const [count, setCount] = useState(0);
   const [hasValue, setHasValue] = useState(false);
-  const [a, seta] = useState(false);
-  const isRemove = a ? 'animate-up' : 'animate-down';
+  const [count, setCount] = useState(0);
+  const [id, setId] = useState(0);
+  const countRef = useRef(1);
+  const todoList = { id: id, text: value, isChecked: false };
+  const isVisible = todo.length === 0 ? 'before:hidden after:hidden' : 'before:block after:block';
 
   const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   const handleAddClick = () => {
-    setCount(count + 1);
-    setTodo([...todo, { id: count, text: value }]);
+    setId(countRef.current++);
+    setTodo([...todo, todoList]);
     setValue('');
     setHasValue(true);
-    seta(true);
   };
 
   const handleEnterKeyDown = (e: React.KeyboardEvent) => {
@@ -28,10 +29,19 @@ export default function ToDo() {
     }
   };
 
-  const handleRemoveClick = (id: number) => {
+  const handleDeleteClick = (id: number) => {
     const updateTodo = todo.filter((item) => item.id !== id);
     setTodo(updateTodo);
   };
+
+  const handleCheckClick = (id: number) => {
+    setTodo((prevTodo) => prevTodo.map((todo) => (todo.id === id ? { ...todo, isChecked: !todo.isChecked } : todo)));
+  };
+
+  useEffect(() => {
+    const unChecked = todo.filter((todo) => !todo.isChecked).length;
+    setCount(unChecked);
+  }, [todo]);
 
   return (
     <>
@@ -43,26 +53,44 @@ export default function ToDo() {
         onKeyDown={handleEnterKeyDown}
       />
       {hasValue && (
-        <div>
-          <ul className='mt-12 m-[0_auto] w-[50rem]'>
+        <div className='mt-12 m-auto w-[50rem] max-md:w-full'>
+          <p
+            className={`relative w-[19rem] m-auto text-center text-primary text-[1.4rem] shadow-[inset_0_-11px_white] ${isVisible} before:absolute before:left-0 before:bottom-3 before:w-8 before:h-8 before:bg-comma before:bg-contain before:scale-[-1] after:absolute after:right-0 after:bottom-2 after:w-8 after:h-8 after:bg-comma after:bg-contain`}
+          >
+            {count !== 0 ? (
+              <>
+                할일이 <span className='font-bold'>{count}</span>개 남았어요
+              </>
+            ) : (
+              todo.length !== 0 && (
+                <>
+                  할일을 모두 <span className='font-bold'>완료</span>했어요
+                </>
+              )
+            )}
+          </p>
+          <ul className='mt-8'>
             {todo.map((todo: Todo, idx) => (
-              <li
-                className={`relative mt-4 first:mt-0 text-[1.25rem] text-primary bg-white px-5 py-4 rounded-3xl ${isRemove}`}
-                key={todo.id}
-              >
+              <li className={`relative mt-4 first:mt-0 bg-white px-5 py-4 rounded-3xl`} key={todo.id}>
                 <input
                   className='absolute left-8 top-[50%] translate-y-[-50%] appearance-none w-10 h-10 bg-checkbox bg-contain bg-no-repeat cursor-pointer checked:bg-checkboxOn'
-                  id={`check${idx + 1}`}
-                  name={`check${idx + 1}`}
+                  id={`checkbox${idx + 1}`}
+                  name={`checkbox${idx + 1}`}
                   type='checkbox'
                 />
-                <label className='ml-20 cursor-pointer' htmlFor={`check${idx + 1}`}>
+                <label
+                  className={`ml-20 max-sm:block max-sm:ml-16 max-sm:w-40 ${
+                    todo.isChecked ? 'opacity-50' : 'opacity-1'
+                  } text-[1.25rem] text-primary cursor-pointer`}
+                  htmlFor={`checkbox${idx + 1}`}
+                  onClick={() => handleCheckClick(todo.id)}
+                >
                   {todo.text}
                 </label>
                 <button
-                  className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-11 h-11 indent-[100%] whitespace-nowrap bg-remove bg-no-repeat bg-contain hover:bg-removeOn'
+                  className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-11 h-11 indent-[100%] whitespace-nowrap bg-delete bg-no-repeat bg-contain hover:bg-deleteOn'
                   type='button'
-                  onClick={() => handleRemoveClick(todo.id)}
+                  onClick={() => handleDeleteClick(todo.id)}
                 >
                   삭제하기
                 </button>
