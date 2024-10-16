86fe1cfa1f42cda243207b72d5cc617a065dfdac
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-17 01:07:39 +0900

    fix:  타입 오류 수정 (#101)
    * feat: 할일 목록 구현
    
    * feat: 삭제 기능 구현
    
    * feat: 애니메이션 추가
    
    * feat: 삭제 버튼 hover 시 효과
    
    * design: checkbox 구현
    
    * fix: checkbox 오류 수정
    
    * feat: 남은 할일 개수 보여주기
    
    * refactor: 각 기능을  컴포넌트로 분리
    
    * feat: toDoList 페이지 구현
    
    * design: UI 수정
    
    * design: 반응형 깨지는 오류 수정
    
    * rename: 컴포넌트명 변경
    
    * fix: value값 비어있을 때 버튼 입력되는 오류 수정
    
    * fix: 타입 오류 수정
    
    * fix: 타입 오류 수정

--- src/pages/components/toDo/ToDoItem.tsx
+++ src/pages/components/toDo/ToDoItem.tsx
@@ -4,13 +4,13 @@ import Check from './Check';
 import { Todo, ToDoProps } from '@/type/type';
 
 export default function ToDoItem({ todo, handleDeleteClick, handleCheckChange, hasValue }: ToDoProps<Todo[]>) {
-  const isVisible = !hasValue && todo && todo.length === 0 && 'invisible';
+  const isVisible = !hasValue && todo.length === 0 && 'invisible';
 
   return (
     <div className={`flex-col items-center gap-10 2xl:flex-row-reverse 2xl:justify-end 2xl:gap-20 flex ${isVisible}`}>
       <Count todo={todo} />
       <ul className='xl:shrink-0 overflow-y-auto w-full md:w-[43.75rem] h-[21.8rem] scrollbar-thumb-primary scrollbar-track-transparent scrollbar-thin'>
-        {todo.map((todo, idx) => (
+        {todo && todo.map((todo, idx) => (
           <li className={`relative mt-4 first:mt-0 bg-white px-5 py-4 rounded-3xl`} key={todo.id}>
             <Check todo={todo} idx={idx} handleCheckChange={handleCheckChange} />
             <Delete todo={todo} handleDeleteClick={handleDeleteClick} />
