a42f9ae39aecc639ef99b9827a0c3e1c20449fda
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-16 15:25:16 +0900

    feat: 애니메이션 추가

--- src/pages/components/to-do/ToDo.tsx
+++ src/pages/components/to-do/ToDo.tsx
@@ -42,7 +42,10 @@ export default function ToDo() {
         <div>
           <ul className='mt-12 m-[0_auto] w-[50rem]'>
             {todo.map((todo: Todo) => (
-              <li className='relative mt-4 text-[1.25rem] text-primary bg-white px-5 py-4 rounded-3xl' key={todo.id}>
+              <li
+                className='relative mt-4 first:mt-0 text-[1.25rem] text-primary bg-white px-5 py-4 rounded-3xl animate-up'
+                key={todo.id}
+              >
                 {todo.text}
                 <button
                   className='absolute top-[50%] right-[2rem] translate-y-[-50%] overflow-hidden w-[50px] h-[50px] indent-[100%] whitespace-nowrap bg-remove bg-no-repeat bg-contain'
