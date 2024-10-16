da0713ef81cd135e8e5c60fe9d54a596c25b1aaa
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-17 00:57:55 +0900

    fix: 클릭시 값 저장안되는 오류 수정

--- src/pages/components/diet/Diet.tsx
+++ src/pages/components/diet/Diet.tsx
@@ -3,25 +3,38 @@ import { DietItems } from '@/type/type';
 import { useDelete } from '@/pages/hooks/useDelete';
 import Input from '../input/Input';
 import Category from './Category';
+import DietItem from './DietItem';
 
 export default function Diet() {
   const [value, setValue] = useState('');
   const [selectedValue, setSelectedValue] = useState('');
   const [diet, setDiet] = useState<DietItems[]>([]);
+  const [filterItem, setFilterItem] = useState<DietItems[]>([]);
   const [id, setId] = useState(0);
-  const [hasItem, setHasItem] = useState(0);
+  const [itemLength, setItemLength] = useState(0);
   const countRef = useRef(1);
-  const { handleDeleteClick } = useDelete(id, diet, setDiet);
+  const { handleDeleteClick } = useDelete(diet, setDiet);
 
   const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  const handleValueClick = (e: MouseEvent<HTMLUListElement>) => {
+  const updateItem = (id: number, update) => {
+    setDiet((prev) => prev.map((item) => (item.id === id ? { ...item, ...update } : item)));
+  };
+
+  const handleAddClick = (e: MouseEvent<HTMLUListElement>) => {
     const target = e.target as HTMLElement;
     const selected = target.getAttribute('data-value');
     if (!selected) return;
-    const dietList = { id: id, text: value, category: selected };
+    const dietList = {
+      id: id,
+      text: value,
+      category: selectedValue,
+      file: '',
+      rating: 0,
+      review: ''
+    };
 
     setSelectedValue(selected);
     setId(countRef.current++);
@@ -34,8 +47,9 @@ export default function Diet() {
   };
 
   useEffect(() => {
-    const itemLength = diet.filter((item: DietItems) => item.category === selectedValue).length;
-    setHasItem(itemLength);
+    const filterItem = diet.filter((item: DietItems) => item.category === selectedValue);
+    setItemLength(filterItem.length);
+    setFilterItem(filterItem);
   }, [diet, selectedValue]);
 
   return (
@@ -45,16 +59,18 @@ export default function Diet() {
         value={value}
         selectedValue={selectedValue}
         onChange={handleValueChange}
-        onClick={handleValueClick}
+        onClick={handleAddClick}
       />
       {selectedValue && (
-        <Category
-          selectedValue={selectedValue}
-          diet={diet}
-          hasItem={hasItem}
-          handleCategoryClick={handleCategoryClick}
-          handleDeleteClick={handleDeleteClick}
-        />
+        <div className='w-[45rem] m-auto'>
+          <Category selectedValue={selectedValue} handleCategoryClick={handleCategoryClick} />
+          <DietItem
+            itemLength={itemLength}
+            filterItem={filterItem}
+            handleDeleteClick={handleDeleteClick}
+            updateItem={updateItem}
+          />
+        </div>
       )}
     </>
   );
