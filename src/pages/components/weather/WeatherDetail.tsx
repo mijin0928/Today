8337e6597a43fd3afc1d72605db4881bee67719d
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-10-17 00:56:17 +0900

    fix: 타입오류 수정 (#99)

--- src/pages/components/weather/WeatherDetail.tsx
+++ src/pages/components/weather/WeatherDetail.tsx
@@ -4,7 +4,7 @@ import { WeatherItem } from '@/type/type';
 export default function WeatherDetail({ weatherDetail }: { weatherDetail: WeatherItem[] }) {
   return (
     <ul className='flex flex-wrap mt-16 gap-5'>
-      {weatherDetail.map((weather, i) => (
+      {weatherDetail && weatherDetail.map((weather, i) => (
         <li
           className='px-5 py-5 rounded-3xl h-auto w-full md:w-[calc(50%_-_0.625rem)] 2xl:w-[calc(20%_-_1.25rem)] shadow-[3px_3px_3px_rgba(0,0,0,0.163)] bg-white'
           key={i}
