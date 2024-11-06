e553da4a25dfd0a1764ac1991dfc1e124e1f5918
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2024-11-07 04:33:44 +0900

    refactor: 불필요한 map 함수 제거

--- src/pages/components/fortune/Result.tsx
+++ src/pages/components/fortune/Result.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getResult } from '@/pages/api/api';
-import { Item } from '@/type/type';
 import Reset from './Reset';
 import Loading from '../loading/loading';
 
@@ -20,16 +19,9 @@ export default function Result() {
   return (
     <>
       <div className='relative flex justify-center items-center h-screen before:absolute before:top-2/4 before:left-2/4 before:translate-x-[-50%] before:translate-y-[-50%] before:w-screen before:h-[90vh] before:bg-paper before:bg-contain before:bg-no-repeat before:bg-center before:z-[-1]'>
-        {resultData.map((item: Item) => {
-          return (
-            <p
-              key={item.id}
-              className='p-4 text-primary text-[1.25rem] bg-white max-sm:text-[1rem] max-md:max-h-[12.5rem] max-sm:max-h-[5rem] max-md:overflow-y-auto scrollbar-thumb-primary scrollbar-track-transparent scrollbar-thin max-w-[45%]'
-            >
-              {item.result}
-            </p>
-          );
-        })}
+        <p className='p-4 text-primary text-[1.25rem] bg-white max-sm:text-[1rem] max-md:max-h-[12.5rem] max-sm:max-h-[5rem] max-md:overflow-y-auto scrollbar-thumb-primary scrollbar-track-transparent scrollbar-thin max-w-[45%]'>
+          {resultData[0].result}
+        </p>
       </div>
       <Reset />
     </>
